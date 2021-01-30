3b4f4cba4324e5865a31233eee9e3b9a24be51e5
Thomas Knowles <thomasa.knowles@gmail.com>
2021-01-30 20:39:27 +1000

    Add WorkTimeView

---
+++ Assets/_Game/Scripts/WorkTimeSystem/WorkTimeView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace GameSystem.WorkTime.UI {
+    public class WorkTimeView : MonoBehaviour {
+
+		[SerializeField] private TextMeshProUGUI timeText;
+
+		public void SetWorkTime(TimeInfo info) {
+			timeText.text = $"{info.Hour}:{info.Minute}";
+		}
+
+	}
+}
