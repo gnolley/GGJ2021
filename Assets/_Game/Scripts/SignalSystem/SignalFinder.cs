95ba9c5e924bfb114801af0a38351cfdee05b80a
Danny <danieltan843@gmail.com>
2021-01-30 15:34:44 +1000

    Add base scripts for signal system

---
+++ Assets/_Game/Scripts/SignalSystem/SignalFinder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SignalSystem {
+
+	/// <summary>
+	///
+	/// </summary>
+	public class SignalFinder : MonoBehaviour {
+
+		public bool GyroAvailable { get; private set; }
+
+		private void Awake() {
+			// check gyro
+			// check system
+		}
+
+		private void Update() {
+			// check gyro
+			// check system
+			// update manager
+		}
+
+		private void NonGyroMovement() {
+
+		}
+	}
+}
---
+++ Assets/_Game/Scripts/SignalSystem/SignalManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace SignalSystem {
+
+	/// <summary>
+	///
+	/// </summary>
+	public class SignalManager : MonoBehaviour {
+
+		#region Singleton
+
+		private static SignalManager instance;
+
+		public static SignalManager Instance {
+			get {
+				Init();
+				return instance;
+			}
+			private set { instance = value; }
+		}
+
+		[RuntimeInitializeOnLoadMethod]
+		private static void Init() {
+			// Unity replaces a destroyed game object with a non-null "destroyed" object.
+			// Using .Equals checks against that.
+			if (instance == null || instance.Equals(null)) {
+				GameObject gameObject = new GameObject("Spawned SignalManager");
+				instance = gameObject.AddComponent<SignalManager>();
+				DontDestroyOnLoad(gameObject);
+
+				Debug.LogFormat("Spawned new {0} singleton", typeof(SignalManager));
+			}
+		}
+
+		#endregion Singleton
+
+		/// <summary>
+		/// The current signal strength [0,1]
+		/// </summary>
+		public float CurrentSignal { get; private set; } = 1;
+
+		// when to shift signal
+		// bounds
+		// relative position
+
+		public void Reset() {
+			// reset variables
+		}
+
+		public void UpdateSignal(float someDirection) {
+			// check against current position
+		}
+	}
+}
