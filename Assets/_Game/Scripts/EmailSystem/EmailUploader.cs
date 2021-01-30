4561314d2acdcff961c6e2397c1a71aa785b5547
Danny <danieltan843@gmail.com>
2021-01-30 15:51:12 +1000

    Add uploader base script

---
+++ Assets/_Game/Scripts/EmailSystem/EmailUploader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace EmailSystem {
+
+	/// <summary>
+	///
+	/// </summary>
+	public class EmailUploader : MonoBehaviour {
+
+		public float UploadProgress { get; private set; }
+
+		public void ResonseChosen(string info) {
+
+		}
+
+		private void StartUpload() {
+			// reset upload progress
+			// start progress meter
+		}
+	}
+}
