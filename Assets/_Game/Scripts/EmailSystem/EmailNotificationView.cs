258d18841e0bd15f7b3a7bd3b478ce6fde2cbe6d
ThomasKnowles <tom@maxart.com.au>
2021-01-30 17:28:04 +1000

    Start Email Notification View

---
+++ Assets/_Game/Scripts/EmailSystem/EmailNotificationView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace EmailSystem.UI {
+	public class EmailNotificationView : MonoBehaviour {
+		[SerializeField] private TextMeshProUGUI title, subject;
+		[SerializeField] private Image portait;
+
+		public void Populate(Email email) {
+			title.text = email.Title;
+			subject.text = email.Subject;
+			portait.sprite = email.Author.Portrait;
+		}
+
+		public void SetAsRead() {
+
+		}
+	}
+}
