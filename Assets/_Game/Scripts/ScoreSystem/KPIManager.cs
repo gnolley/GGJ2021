8ec47eac162ea46821e89df2105f340ac6227de0
Danny <danieltan843@gmail.com>
2021-01-30 15:43:09 +1000

    Add base score manager

---
+++ Assets/_Game/Scripts/ScoreSystem/KPIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class KPIManager : MonoBehaviour {
+
+	/// <summary>
+	/// Key performance indicator [0,100?]
+	/// </summary>
+	public int KPI { get; private set; }
+
+	public void AddScore(int score) {
+		// some way of calculating score to add to kpi
+	}
+}
