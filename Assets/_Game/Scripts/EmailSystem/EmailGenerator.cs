14dba454fb9d7765c709db561cbfcc725ce70171
Thomas Knowles <thomasa.knowles@gmail.com>
2021-01-30 22:57:35 +1000

    Added in generating dummy emails

--- Assets/_Game/Scripts/EmailSystem/EmailGenerator.cs
+++ Assets/_Game/Scripts/EmailSystem/EmailGenerator.cs
@@ -2,13 +2,28 @@
 
 namespace EmailSystem {
 
-	/// <summary>
-	///
-	/// </summary>
 	public class EmailGenerator : MonoBehaviour {
 
-		private Email GenerateEmail(EmailType type) {
-			return null;
+		public Email GenerateInfoEmail() { 
+			return new InfoEmail(DEFAULT_AUTHOR, "Info Email", "Some more Info", DEFAULT_BODY, new EmailInfo("10:30")); 
 		}
+		public Email GenerateInquiryEmail(EmailInfo basedOn) {
+			return new InquiryEmail(DEFAULT_AUTHOR, "Inquiry Email", "Some Inquiry Please", DEFAULT_BODY, basedOn);
+		}
+		public Email GenerateResponseEmail(Email basedOn) {
+			return new ResponseEmail(DEFAULT_AUTHOR, "Response Email", "You Suck!", DEFAULT_BODY, basedOn);
+		}
+		public Email GenerateSpamEmail() {
+			return new SpamEmail(DEFAULT_AUTHOR, "Spam Email", "Give me MoneY!", DEFAULT_BODY);
+		}
+
+
+		Author DEFAULT_AUTHOR = new Author();
+
+		const string DEFAULT_BODY = 
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+			"Praesent eu auctor nisl, vitae molestie nibh. " +
+			"Proin feugiat id mi quis laoreet. Ut vitae neque magna. " +
+			"Interdum et malesuada fames ac ante orci.";
 	}
 }
