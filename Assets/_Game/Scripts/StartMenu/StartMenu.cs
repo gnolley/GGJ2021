e00e539ad963237247848ac96ab3348bef4e253c
Danny <danieltan843@gmail.com>
2021-01-31 15:03:26 +1000

    Add UI to start menu, Add scene load on start menu button

---
+++ Assets/_Game/Scripts/StartMenu/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+///
+/// </summary>
+public class StartMenu : MonoBehaviour {
+
+	[SerializeField, Scene] private string gameScene;
+
+	public void OnPlayButton() {
+		SceneManager.LoadScene(gameScene);
+	}
+
+}
